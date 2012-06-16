99595d5f1e1a2456a86bb29a6dfc07a59dfa4aa3
sdepold <sascha@depold.com>
2012-06-16 21:17:52 +0200

    specs are working

--- spec/jquery.google-search.spec.js
+++ spec/jquery.google-search.spec.js
@@ -1,12 +1,36 @@
+buster.testRunner.timeout = 10000
 buster.spec.expose()
 
 describe('jquery.google-search', function() {
-  before(function() {
-    this.element = jQuery('<div>').appendTo(jQuery('body'))
+
+
+  describe('search', function() {
+    beforeAll(function() {
+      this.googleSearch = new $.GoogleSearch()
+    })
+
+    it("returns results which have the searched keyword included", function(done) {
+      console.log('fix empty result row')
+
+      this.googleSearch.search('foo', {}, function(data) {
+        data.forEach(function(row) {
+          if((row.url !== undefined) && (row.url !== 'undefined')) {
+            expect(row.description.toLowerCase().indexOf('foo')).not.toEqual(-1)
+          }
+        })
+        done()
+      })
+    })
   })
 
-  it('prints "google-search"', function() {
-    this.element.googleSearch()
-    expect(this.element.text()).toEqual('google-search')
+  describe('cleanUp', function() {
+    beforeAll(function() {
+      this.googleSearch = new $.GoogleSearch()
+    })
+
+    it("cleans up the DOM", function() {
+      this.googleSearch.cleanUp()
+      expect($('body > *').length).toEqual(0)
+    })
   })
 })
